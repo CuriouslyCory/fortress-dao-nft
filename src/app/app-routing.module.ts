32bec8a1a0943e1c1f923b0c3fd38ab1be4a9359
Cory Sougstad <cory@hau.me>
2021-12-15 21:00:57 -0700

    Added body items

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './components/pages/main/main.component';
+import { PageNotFoundComponent } from './components/pages/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'main', component: MainComponent},
+  { path: '',   redirectTo: '/main', pathMatch: 'full' }, // redirect to `first-component`
+  { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { CharacterDisplayComponent } from './components/character-display/character-display.component';
+import { MainComponent } from './components/pages/main/main.component';
+import { PageNotFoundComponent } from './components/pages/page-not-found/page-not-found.component';
+import { GenderSelectorComponent } from './components/gender-selector/gender-selector.component';
+import { TraitChooserComponent } from './components/trait-chooser/trait-chooser.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CharacterDisplayComponent,
+    MainComponent,
+    PageNotFoundComponent,
+    GenderSelectorComponent,
+    TraitChooserComponent
   ],
   imports: [
     BrowserModule,
